43d7e99f981a6e1e940dce69e91f0da686fc0151
fan <852672780@qq.com>
2017-05-30 01:09:53 +0800

    return sum

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
+module.exports = function main(String s) {
   // Write your cade here
+  var i=s.length();
+  var x=parseInt(s);
+  var sum=0;
+  for(var n=0;n<i;n++){
+	  sum+=x%10;
+	  x=x/10;
+  }
+  return sum;
 };
