3c6f081a8caeb36815f6189ae2040a67f1688958
Fanzhikang <852672780@qq.com>
2017-05-30 16:32:41 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,11 +1,4 @@
-module.exports = function main(String s) {
+module.exports = function main() {
   // Write your cade here
-  var i=s.length();
-  var x=parseInt(s);
-  var sum=0;
-  for(var n=0;n<i;n++){
-	  sum+=x%10;
-	  x=x/10;
-  }
-  return sum;
+ 
 };
